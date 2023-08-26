23cef9be7a354e57f1d0b5179ab23a532372eda1
“laxminarayanaboga4079@gmail.com” <laxminarayanaboga4079@gmail.com>
2023-08-26 22:59:06 +0100

    added student rest service

---
+++ src/main/java/com/luv2code/demo/Luv2codeApplication.java
@@ -0,0 +1,12 @@
+package com.luv2code.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Luv2codeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Luv2codeApplication.class, args);
+    }
+}
---
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -0,0 +1,37 @@
+package com.luv2code.demo.rest;
+
+import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadStudents() {
+        students = new ArrayList<>();
+        students.add(new Student("first", "person"));
+        students.add(new Student("second", "person2"));
+        students.add(new Student("third", "person3"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/student/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
+    }
+
+}
